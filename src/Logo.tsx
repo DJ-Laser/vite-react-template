1afec32740f81ed8bb1878c15f86e333a11f436d
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-12-07 23:57:07 -0500

    apply eslint fix

--- src/Logo.tsx
+++ src/Logo.tsx
@@ -5,7 +5,7 @@ interface LogoProps {
 }
 
 export function Logo({ href, src, alt }: LogoProps) {
-  return <a href={href} target="_blank">
+  return <a href={href} target="_blank" rel="noreferrer">
     <img src={src} className="h-36 p-6" alt={alt} />
   </a>
 }
